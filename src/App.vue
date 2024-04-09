a22b76e9004e8e56ae1099e18929f94225e9b004
sebik9 <s3bik9@seznam.cz>
2024-04-09 13:00:43 +0200

    validation update
    added checks for required ´fields

--- src/App.vue
+++ src/App.vue
@@ -7,80 +7,91 @@
       <h1>Ticketino</h1>
     </header>
     <form>
-      <div class="row">
-        <div class="field">
-          <label for="first-name" class="name">First Name</label>
-          <input type="text" id="first-Name" v-model="firstName" />
-        </div>
-        <div class="field">
-          <label for="last-name" class="name">Last Name</label>
-          <input type="text" id="last-name" v-model="lastName" />
-        </div>
-      </div>
-      <div class="row">
-        <div class="field-email">
-          <label for="email">Email</label>
-          <input type="text" id="email" v-model="email" />
-        </div>
-      </div>
-      <div class="row">
-        <div class="field">
-          <label for="ticket-quantity">Ticket Quantity</label>
-          <select id="ticket-quantity" v-model="ticketQuant">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-          </select>
-        </div>
-        <div class="field">
-          <legend>Ticket Type</legend>
-          <div>
-            <input type="radio" id="standard" value="standard" v-model="ticketType" />
-            <label for="standard">Standard</label>
+      <div class="bgc">
+        <div class="row">
+          <div class="field">
+            <label for="first-name" class="name">First Name</label>
+            <input type="text" id="first-Name" v-model="firstName" @input="validateForm" />
           </div>
-          <div>
-            <input type="radio" id="vip" value="vip" v-model="ticketType" />
-            <label for="vip">VIP</label>
+          <div class="field">
+            <label for="last-name" class="name">Last Name</label>
+            <input type="text" id="last-name" v-model="lastName" @input="validateForm" />
           </div>
         </div>
-      </div>
-      <div class="row checkbox">
-        <fieldset>
-          <legend>How did you hear about this Event?</legend>
-          <div>
-            <input type="checkbox" id="friend" value="friend" v-model="referral" />
-            <label for="friend">From a friend</label>
+        <div class="row">
+          <div class="field-email">
+            <label for="email">Email</label>
+            <input type="text" id="email" v-model="email" @input="validateForm" />
           </div>
-          <div>
-            <input type="checkbox" id="television" value="television" v-model="referral" />
-            <label for="television">On television</label>
-          </div>
-          <div>
-            <input type="checkbox" id="social-media" value="socialMedia" v-model="referral" />
-            <label for="social-media">Social Media</label>
-          </div>
-        </fieldset>
-      </div>
-      <div class="row">
-        <div class="field-email">
-          <label for="special" class="special-label">Special requests:</label>
-          <textarea id="special" v-model="specialRequest"></textarea>
         </div>
-      </div>
-      <div class="row">
-        <fieldset>
-          <legend>Purchase Agreement</legend>
-          <p>I, PH, wish to buy PH. I understand tha all ticket sales are final.</p>
-          <input type="checkbox" name="agreement" id="agree" v-model="agreementBox" />
-          <label for="agree">I Agree</label>
-        </fieldset>
-      </div>
-      <div class="row">
-        <button type="reset" class="reset-btn">Reset</button>
-        <button type="submit" class="confirm-btn">Confirm order</button>
+        <div class="row">
+          <div class="field">
+            <label for="ticket-quantity">Ticket Quantity</label>
+            <select id="ticket-quantity" v-model="ticketQuant">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+            </select>
+          </div>
+          <div class="field">
+            <legend>Ticket Type</legend>
+            <div>
+              <input type="radio" id="standard" value="standard" v-model="ticketType" />
+              <label for="standard">standard</label>
+            </div>
+            <div>
+              <input type="radio" id="vip" value="vip" v-model="ticketType" />
+              <label for="vip">VIP</label>
+            </div>
+          </div>
+        </div>
+        <div class="row checkbox">
+          <fieldset>
+            <legend>How did you hear about this Event?</legend>
+            <div>
+              <input type="checkbox" id="friend" value="friend" v-model="referral" />
+              <label for="friend">From a friend</label>
+            </div>
+            <div>
+              <input type="checkbox" id="television" value="television" v-model="referral" />
+              <label for="television">On television</label>
+            </div>
+            <div>
+              <input type="checkbox" id="social-media" value="socialMedia" v-model="referral" />
+              <label for="social-media">Social Media</label>
+            </div>
+          </fieldset>
+        </div>
+        <div class="row">
+          <div class="field-email">
+            <label for="special" class="special-label">Special requests:</label>
+            <textarea id="special" v-model="specialRequest"></textarea>
+          </div>
+        </div>
+        <div class="row">
+          <fieldset>
+            <legend>Purchase Agreement</legend>
+            <p class="agree-text">
+              I, PH, wish to buy {{ ticketType }} ticket. I understand tha all ticket sales are
+              final.
+            </p>
+            <input
+              type="checkbox"
+              name="agreement"
+              id="agree"
+              v-model="agreementBox"
+              @change="validateForm"
+            />
+            <label for="agree">I Agree</label>
+          </fieldset>
+        </div>
+        <div class="row">
+          <button type="reset" class="reset-btn" @click="formReset">Reset</button>
+          <button type="submit" class="confirm-btn" :disabled="!isValid">Confirm order</button>
+        </div>
       </div>
     </form>
   </body>
@@ -94,7 +105,9 @@
     {{ ticketType }}<br />
     {{ referral }}<br />
     {{ specialRequest }}<br />
-    {{ agreementBox }}
+    {{ agreementBox }}<br />
+    {{ fullName }}<br />
+    {{ isValid }}
   </div>
 </template>
 
@@ -106,20 +119,47 @@ export default {
       lastName: '',
       email: '',
       ticketQuant: 1, //default value of 1
-      ticketType: 'Standard', // or VIP
+      ticketType: 'standard', // or VIP
       referral: [],
       specialRequest: '',
-      agreementBox: false //default value of false user has to agree manualy
+      agreementBox: false, //default value of false user has to agree manualy
+      isValid: false
     }
   },
-  computed: {},
-  methods: {}
+  computed: {
+    fullName() {
+      return this.firstName.trim() + ' ' + this.lastName.trim()
+    }
+  },
+  methods: {
+    validateForm() {
+      if (this.firstName && this.lastName && this.email && this.agreementBox) {
+        this.isValid = true
+      } else {
+        this.isValid = false
+      }
+    },
+    formReset() {
+      this.firstName = ''
+      this.lastName = ''
+      this.email = ''
+      this.ticketQuant = 1
+      this.referral = []
+      this.specialRequest = ''
+      this.agreementBox = false
+      this.validateForm()
+    }
+  }
 }
 </script>
 
 <style scoped>
 body {
   max-width: 800px;
+}
+
+.bgc {
+  background-color: #4c7ef3;
 }
 
 header {
@@ -150,37 +190,32 @@ form {
 .row {
   display: flex;
   flex-direction: row;
-
-  background-color: pink;
   margin-top: 5px;
   margin-bottom: 5px;
 }
 
 .field {
   width: 50%;
-  background-color: red;
 }
 
 .field-email {
   width: 100%;
-  background-color: palegreen;
+  min-width: 239px;
 }
 
 #email {
-  width: calc(100% - 26px);
-  background-color: purple;
+  width: calc(100% - 70px);
 }
 
 #special {
   width: calc(100% - 6px);
-  max-width: calc(100% - 6px);
+  max-width: calc(100% - 47px);
   max-height: 90px;
   min-height: 30px;
-  background-color: purple;
+  margin-top: 5px;
 }
 
 input {
-  background-color: blue;
   margin-right: 15px;
   padding: 0.71rem;
   margin-top: 5px;
@@ -188,6 +223,7 @@ input {
 
 fieldset {
   border: none;
+  margin-left: -12px;
 }
 
 select {
@@ -206,6 +242,10 @@ select {
   width: 100%;
 }
 
+.agree-text {
+  max-width: 440px;
+}
+
 @media (max-width: 734px) {
   .row {
     flex-direction: column;
@@ -215,7 +255,6 @@ select {
   }
   .name {
     width: 100%;
-    background-color: yellow;
     float: left;
   }
 }
